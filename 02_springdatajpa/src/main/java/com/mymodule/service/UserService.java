a7c337d13c209424f133e9632ec1fe3b44121124
aa <aa@qq.com>
2019-10-27 18:16:46 +0800

    aa

---
+++ 02_springdatajpa/src/main/java/com/mymodule/service/UserService.java
@@ -0,0 +1,10 @@
+package com.mymodule.service;
+
+public class UserService {
+
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        System.out.println("bbbb");
+        System.out.println("cccc");
+    }
+}
